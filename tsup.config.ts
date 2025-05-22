6e640139e0b9c1c21281bf4a5da7a24e9aa6793b
Krishna Kapoor <kapoorkrishna846@gmail.com>
2025-05-22 15:33:22 +0200

    Possible ctx.tsx patch

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/core/index.ts', 'src/react.ts', 'src/ctx.tsx'],
-  format: ['esm', 'cjs'],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  splitting: true,
-  target: 'es2020',
-  outDir: 'dist',
-  minify: true,
-});
+export default defineConfig([
+    {
+        entry: ['src/core/index.ts', 'src/react.ts'],
+        format: ['esm', 'cjs'],
+        dts: true,
+        sourcemap: true,
+        clean: true,
+        splitting: true,
+        target: 'es2020',
+        outDir: 'dist',
+    },
+    {
+        entry: ['src/ctx.tsx'],
+        format: ['esm', 'cjs'],
+        dts: true,
+        sourcemap: true,
+        clean: true,
+        splitting: true,
+        target: 'es2020',
+        outDir: 'dist',
+        banner: {
+            js: `'use client';`,
+        },
+    },
+]);
